24311a4b0c1d1c8ff641adb7b28325afaaf45eca
Jesi <62769448+jessiecamm@users.noreply.github.com>
2020-06-11 13:33:54 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -130,7 +130,8 @@
                                             <?php echo "$" . $_POST['unit'] ?>
                                         </h3>
                                     </div>
-                                    <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
+                                    <button type="submit" class="mercadopago-button" formmethod="post">Pagar la
+compra</button>
                                 </div>
                             </div>
                         </div>
